d8778c7b327926dd31832dfc10adbb29b642b1ad
Sarvagya Sharma <sarvagya2545@gmail.com>
2020-12-10 23:12:34 +0530

    Added sent time feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,5 @@
-var socket = io.connect('https://enigmatic-tundra-59197.herokuapp.com/')
+// var socket = io.connect('https://enigmatic-tundra-59197.herokuapp.com/')
+var socket = io.connect('http://localhost:3000/')
 
 const btnSend = document.getElementById('btn-send')
 const chatInput = document.getElementById('chat-input')
@@ -10,8 +11,10 @@ btnSend.addEventListener('click', e => {
     e.preventDefault()
     const message = chatInput.value
     if(message !== '') {
+        var date = new Date()
         var chatMsg = {
             message: message,
+            date: date,
             id: socket.id
         }
         socket.emit('message sent', chatMsg)
@@ -31,9 +34,29 @@ const addMessage = (chatMsg) => {
     } else {
         msgBox.classList.add('others')
     }
-    msgBox.textContent = chatMsg.message            
+    msgBox.innerHTML = 
+    `${chatMsg.message}
+        <span class="chat-message-time">
+            Sent: ${formatAMPM(chatMsg.date)}
+        </span>
+    `
+
     li = document.createElement('li')
     li.className = 'chat-list-item'
     li.appendChild(msgBox)    
+    console.log(li)
     chatBox.appendChild(li)
+}
+
+function formatAMPM(date) {
+    date = new Date(date)
+    console.log(date)
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
 }
